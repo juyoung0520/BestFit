a2b655467dea44d6dd477ea6f631c786125f6ade
진주영 <juyoung0520@naver.com>
2020-05-30 01:21:56 +0900

    프래그먼트 만들기.

--- app/src/main/java/com/example/bestfit/SignupActivity.kt
+++ app/src/main/java/com/example/bestfit/SignupActivity.kt
@@ -10,5 +10,7 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        supportFragmentManager.beginTransaction().replace(R.id.activity_signup_layout_frame, SignupFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/bestfit/SignupFragment.kt
@@ -0,0 +1,27 @@
+package com.example.bestfit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_signup.*
+import kotlinx.android.synthetic.main.fragment_signup.view.*
+
+class SignupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_signup, container, false)
+
+        view.fragment_signup_btn_next.setOnClickListener{
+
+            (activity as SignupActivity).supportFragmentManager.beginTransaction().replace(R.id.activity_signup_layout_frame, ProfileFragment()).commit()
+        }
+
+        return view
+    }
+}
