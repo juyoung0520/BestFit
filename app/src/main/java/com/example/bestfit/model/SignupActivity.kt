bc1731a21c175c2f114075b7d78a02b8e3eece53
진주영 <juyoung0520@naver.com>
2020-05-29 15:25:05 +0900

    회원가입 레이아웃, 프래그먼트, 액티비티 만들었습니당~

---
+++ app/src/main/java/com/example/bestfit/model/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bestfit.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bestfit.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
