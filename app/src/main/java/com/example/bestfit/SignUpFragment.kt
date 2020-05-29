5926a063cc9c8cb3acdabc329c6499ec2e5bc2f9
j1nu <gle00@naver.com>
2020-05-30 02:11:23 +0900

    SignUpFragment 이름 수정

--- app/src/main/java/com/example/bestfit/SignUpFragment.kt
+++ app/src/main/java/com/example/bestfit/SignUpFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_signup.view.*
 
-class SignupFragment : Fragment() {
+class SignUpFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
