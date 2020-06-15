fd8a551dade9cfc677b8b2b0006afcf55286e821
j1nu <gle00@naver.com>
2020-06-16 03:19:10 +0900

    add AutoCompleteTextView in AddItemFirst

--- app/src/main/java/com/example/bestfit/AddItemFirstFragment.kt
+++ app/src/main/java/com/example/bestfit/AddItemFirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_add_item_first.view.*
 import kotlinx.android.synthetic.main.spinner_item.view.*
@@ -21,6 +22,8 @@ class AddItemFirstFragment  : Fragment() {
 
         initCategorySpinner(view)
 
+        test(view)
+
         return view
     }
 
@@ -28,6 +31,43 @@ class AddItemFirstFragment  : Fragment() {
     // https://www.it-swarm.dev/ko/android/android%EC%97%90%EC%84%9C-spinner%EB%A5%BC-%EB%A7%9E%EC%B6%A4-%EC%84%A4%EC%A0%95%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/1073376298/
 
 //    accountDTO.skip = true
+
+    fun test(view: View) {
+        var categoryAdapter = object : ArrayAdapter<String>(context!!, R.layout.spinner_item) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val v = super.getView(position, convertView, parent)
+
+                if (position == count) {
+                    v.spinner_item_tv.text = ""
+                    v.spinner_item_tv.hint = getItem(count)
+                }
+
+                return v
+            }
+
+            override fun getCount(): Int {
+                return super.getCount() - 1
+            }
+        }
+
+        val categoryDTOs = InitData.categoryDTOs
+        categoryDTOs.removeAt(0)
+
+        val categories = InitData.categories
+        categories.removeAt(0)
+
+        categoryAdapter.addAll(categories)
+        categoryAdapter.add("대분류")
+
+        view.fragment_add_item_first_actv_category.setAdapter(categoryAdapter)
+        view.fragment_add_item_first_actv_category.keyListener = null
+        view.fragment_add_item_first_actv_category.setOnTouchListener { v, event ->
+            (v as AutoCompleteTextView).showDropDown()
+            false
+        }
+
+//        view.fragment_add_item_first_actv_category.setSelection(categoryAdapter.count)
+    }
 
     fun initCategorySpinner(view: View) {
         val categoryAdapter = object : ArrayAdapter<String>(context!!, R.layout.spinner_item) {
