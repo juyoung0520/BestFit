251c5748e40bdf02d09941d37fa9567f865de801
j1nu <gle00@naver.com>
2020-08-15 13:09:00 +0900

    add NavigationFragmentViewModel

--- app/src/main/java/com/example/bestfit/viewmodel/NavigationFragmentViewModel.kt
+++ app/src/main/java/com/example/bestfit/viewmodel/NavigationFragmentViewModel.kt
@@ -6,16 +6,24 @@ import androidx.lifecycle.ViewModel
 
 class NavigationFragmentViewModel : ViewModel() {
     private val navigationFragments = MutableLiveData<ArrayList<ArrayList<Fragment>>>()
+    private var currentNavigationIndex = MutableLiveData<Int>(0)
 
-    fun getNavigationFragments(index: Int) : ArrayList<Fragment>? {
-        if (index < 0 || index > navigationFragments.value?.size ?: 0)
+    fun getNavigationFragments(navigationIndex: Int) : ArrayList<Fragment>? {
+        if (navigationIndex < 0 || navigationIndex > navigationFragments.value?.size ?: 0)
             return null
 
-        return navigationFragments.value?.get(index) as ArrayList<Fragment>
+        return navigationFragments.value!![navigationIndex]
     }
 
-    fun addNavigationFragments() {
-//    currentNavigation[newNavigationIndex].add(newFragment!!)
+    fun addNavigationFragment(navigationIndex: Int, fragment: Fragment) {
+        navigationFragments.value!![navigationIndex].add(fragment)
+    }
 
+    fun removeNavigationFragment(navigationIndex: Int, index: Int) {
+        navigationFragments.value!![navigationIndex].removeAt(index)
+    }
+
+    fun changeCurrentNavigation(navigationIndex: Int) {
+        currentNavigationIndex.value = navigationIndex
     }
 }
