04c9b928d58d7351407330b25dfeb8537943e9be
Juyoung Jin <juyoung0520@naver.com>
2020-08-28 03:00:56 +0900

    add dibsFragmentViewModel

--- app/src/main/java/com/example/bestfit/DibsFragment.kt
+++ app/src/main/java/com/example/bestfit/DibsFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.example.bestfit.model.ItemDTO
+import com.example.bestfit.viewmodel.DibsFragmentViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 class DibsFragment : Fragment() {
+    private lateinit var viewModel: DibsFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +28,13 @@ class DibsFragment : Fragment() {
     }
 
     private fun initViewModel(view: View) {
+        viewModel = ViewModelProvider(this).get(DibsFragmentViewModel::class.java)
 
+        val initItemDTOsObserver = Observer<ArrayList<ItemDTO>> {itemDTOs ->
+
+        }
+
+        viewModel.itemDTOs.observe(viewLifecycleOwner, initItemDTOsObserver)
     }
 
 }
