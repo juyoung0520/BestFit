36851956e776c2764a100c50ee402a8544c9928c
Juyoung Jin <juyoung0520@naver.com>
2020-08-28 01:38:10 +0900

    add dibsFragment

---
+++ app/src/main/java/com/example/bestfit/DibsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.bestfit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+
+class DibsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+        ): View? {
+        val view = inflater.inflate(R.layout.fragment_dibs, container, false)
+
+        initViewModel(view)
+
+        return view
+    }
+
+    private fun initViewModel(view: View) {
+
+    }
+
+}
