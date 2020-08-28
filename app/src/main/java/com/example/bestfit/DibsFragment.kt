8de7bab8eecb919960b227ba2bc7bf42865fa4d7
Juyoung Jin <juyoung0520@naver.com>
2020-08-28 17:57:41 +0900

    add dibs recyclerview

--- app/src/main/java/com/example/bestfit/DibsFragment.kt
+++ app/src/main/java/com/example/bestfit/DibsFragment.kt
@@ -7,14 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.bestfit.model.ItemDTO
 import com.example.bestfit.viewmodel.DibsFragmentViewModel
+import com.example.bestfit.viewmodel.DressroomCategoryFragmentViewModel
+import kotlinx.android.synthetic.main.fragment_dressroom_category.view.*
+import kotlinx.android.synthetic.main.item_dressroom.view.*
 import java.util.*
 import kotlin.collections.ArrayList
 
 class DibsFragment : Fragment() {
     private lateinit var viewModel: DibsFragmentViewModel
+    private lateinit var dressroomCategoryFragmentViewModel: DressroomCategoryFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +45,61 @@ class DibsFragment : Fragment() {
         }
 
         viewModel.itemDTOs.observe(viewLifecycleOwner, initItemDTOsObserver)
+    }
+
+//    private fun initRecyclerView(view: View) {
+//        dressroomCategoryFragmentViewModel = ViewModelProvider(this).get(DressroomCategoryFragmentViewModel::class.java)
+//
+//        if (dressroomCategoryFragmentViewModel.getItemRecyclerViewAdapter() == null) {
+//            itemRecyclerViewAdapter = ItemRecyclerViewAdapter()
+//            dressroomCategoryFragmentViewModel.setItemRecyclerViewAdapter(itemRecyclerViewAdapter)
+//        }
+//        else
+//            itemRecyclerViewAdapter = dressroomCategoryFragmentViewModel.getItemRecyclerViewAdapter()!!
+//
+//        layoutManager = GridLayoutManager(context, 2)
+//        view.fragment_dressroom_category_recyclerview.adapter = itemRecyclerViewAdapter
+//        view.fragment_dressroom_category_recyclerview.layoutManager = layoutManager
+//    }
+
+    inner class ItemRecyclerViewAdapter: ListAdapter<ItemDTO, ItemRecyclerViewAdapter.ItemViewHolder> {
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ItemRecyclerViewAdapter.ItemViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(
+            holder: ItemRecyclerViewAdapter.ItemViewHolder,
+            position: Int
+        ) {
+            TODO("Not yet implemented")
+        }
+
+        inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(itemDTO: ItemDTO) {
+                val view = itemView
+
+                Glide.with(view)
+                    .load(itemDTO.images!![0])
+                    .apply(RequestOptions().centerCrop())
+                    .into(view.item_dressroom_iv_item)
+
+                view.item_dressroom_tv_item_name.text = itemDTO.name
+                view.setOnClickListener {
+                    val navController = findNavController()
+                    val action = when (navController.currentDestination!!.id) {
+                        R.id.dressroomFragment -> DressroomFragmentDirections.actionToDetailFragment(itemDTO)
+                        R.id.accountFragment -> AccountFragmentDirections.actionToDetailFragment(itemDTO)
+                        else -> null
+                    }
+
+                    navController.navigate(action!!)
+                }
+            }
+        }
     }
 
 }
